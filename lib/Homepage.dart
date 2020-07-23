c880633181f850c099529525885d0cbc7a3f2fcf
daveedson <daveeddrumz@gmail.com>
2020-07-23 12:02:13 +0100

    Added different UI States

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -18,6 +18,48 @@ class _HomePageState extends State<HomePage> {
 
   final _controller = TextEditingController();
 
+  //this method checks for different Ui states using an enum in the NewsArticleListViewModelClass.
+  Widget _buildItems(NewsArticleListViewModel viewModel) {
+    switch (viewModel.loadingStatus) {
+      case LoadingStatus.searching:
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+
+      case LoadingStatus.empty:
+        return Expanded(
+          child: Container(
+            height: 400,
+            width: 300,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Image.asset('images/cherry.png'),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Text(
+                    'No Results Found',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w600),
+                  )
+                ],
+              ),
+            ),
+          ),
+        );
+      case LoadingStatus.completed:
+        return Expanded(
+          child: NewsList(articles: viewModel.articles),
+        );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final viewModel =
@@ -42,9 +84,7 @@ class _HomePageState extends State<HomePage> {
                     )),
               ),
             ),
-            Expanded(
-              child: NewsList(articles: viewModel.articles),
-            ),
+            _buildItems(viewModel),
           ],
         ));
   }
--- lib/ViewModels/NewsArticleListViewModel.dart
+++ lib/ViewModels/NewsArticleListViewModel.dart
@@ -3,31 +3,42 @@ import 'package:newsapp/ViewModels/NewsArticleViewModel.dart';
 import 'package:newsapp/models/newsArticle.dart';
 import 'package:newsapp/services/Webservices.dart';
 
+enum LoadingStatus {
+  completed,
+  searching,
+  empty,
+}
+
 class NewsArticleListViewModel extends ChangeNotifier {
+  var loadingStatus = LoadingStatus.searching;
+
   //Instance creation of NewsArticleViewModel
   // also created a list of NewsArticleViewModel
   List<NewsArticleViewModel> articles = new List<NewsArticleViewModel>();
 
-//  NewsArticleListViewModel() {
-//    _populateHeadlines();
-//  }
-
   //this function is responsible for populating the top headlines..
   Future<void> populateHeadlines() async {
+    this.loadingStatus = LoadingStatus.searching;
+
     List<NewsArticle> newsArticles = await WebService().fetchTopHeadlines();
 
     this.articles = newsArticles
         .map((article) => NewsArticleViewModel(newsArticle: article))
         .toList();
+    this.loadingStatus =
+        this.articles.isEmpty ? LoadingStatus.empty : LoadingStatus.completed;
     notifyListeners();
   }
 
   //this method enables search of different news with there key words..
   Future<void> searchLatestNews(String keyword) async {
+    this.loadingStatus = LoadingStatus.searching;
     List<NewsArticle> searchNews = await WebService().searchHeadlines(keyword);
     this.articles = searchNews
         .map((newsArticles) => NewsArticleViewModel(newsArticle: newsArticles))
         .toList();
+    this.loadingStatus =
+        this.articles.isEmpty ? LoadingStatus.empty : LoadingStatus.completed;
     notifyListeners();
   }
 }
